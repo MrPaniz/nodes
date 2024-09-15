2f91c84b97bb8967671987de87fc80d479ce73f1
MrPaniz <teo.panizzo@gmail.com>
2024-09-15 18:47:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const express = require('express');
-require('dotenv').config();
-const apiKey = process.env.API_KEY 
-const app = express(); 
-const cors = require('cors');
-app.use(cors())
-app.use(express.json());
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(apiKey || process.env.API_KEY);
+const express = require("express");//
+require('dotenv').config()//
+const querystring = require('querystring');//
+const axios = require("axios");//
+const SpotifyWebApi = require('spotify-web-api-node');//
+const cors = require('cors');//
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 80; 
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-
+// Configurazione delle variabili di ambiente
 const clientId = process.env.ClientId;
 const clientSecret = process.env.ClientSecret;
-const redirect_uri = 'https://estensione.onrender.com/callback'; // Modifica l'URL di redirect appropriato
+const redirect_uri = 'http://localhost/callback'; // Modifica l'URL di redirect appropriato
+
+let spotifyApi;
+//chiamata per loop a se stesso. 
 
 
+app.use(express.static('public'));
+app.use(express.json());
 
+// Gestione del login con Spotify
 app.get('/login', (req, res) => {
-  const state =  "byuefueioqfefwpo"
-  const scope = 'user-read-private user-read-email user-modify-playback-state';
+    const state =  "byuefueioqfefwpo"
+    const scope = 'user-read-private user-read-email user-modify-playback-state';
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-      response_type: 'code',
-      client_id: clientId,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-  }));
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+    }));
 });
 
 app.get('/', (req, res) => {
-  res.status(200).send('Ok');
-  
+    res.status(200).send('Ok');
+    
 })
+
 
 // Callback dopo il login
 app.get('/callback', async (req, res) => {
@@ -90,41 +93,20 @@ app.post('/track', async (req, res) => {
   }
 });
 
-
-app.get('/ailink', async (req, res) => {  
-  lan = req.query.lan;
-  link = req.query.link;  
-  var text = await getAiData(lan, link);
-  res.status(200).json({"text": text});
-})
-
-async function getAiData(lang = "italiano", link) {
-  const prompt = `fai un riassunto, in ${lang}, di massimo 600 caratteri del contenuto all'interno di questo link: '${link}'. Non evidenziare il fatto che la risposta l'hai trovata li; scrivi solo la risposta`;
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  return text;
-}
+// Avvia il server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
 
 
-app.get('/aitrovaq', async (req, res) => {
-  lan = req.query.lan ?? "italiano";
-  link = req.query.link
-  q = req.query.q;
-  data = await  axios.get(link)
-  const textPage = cheerio.load(data).text();
-  const prompt = `rispondi, in ${lan}, a "${q}" con un massimo 400 caratteri basandoti solo sul contenuto all'interno di questo link: '${link}' e di '${textPage}', considerando che in quest ultimo ci può essere del codice html. Non è necessario ci sia una risposta precisa, se la risposta, non è presente in nessun modo, fallo sapere e dai tu una risposta sempre non superando 400 caratteri.
-  inoltre se c'è la risposta non evidenziare il fatto che la risposta l'hai trovata li; scrivi solo la risposta o soluzione trovata all'interno del link o del testo.
-  `;
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text()
-  res.status(200).json({"text": text});
-})
+const makeRequest = async () => {
+    try {
+      await axios.get('http://localhost/');
+        console.log('GET request to localhost successful');
+    } catch (error) {
+      console.error('Error making GET request:', error.message);
+    }
+  };
 
-app.get('/prova', (req, res) => {
-res.json({text: "prova"});
-})
-
-
-app.listen(80);
+  const interval = setInterval(makeRequest, 5000);
+//
